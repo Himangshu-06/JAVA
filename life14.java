4fa768654cf686363615c2d1e9d21879b69d3024
Himangshu-06 <keothimangshu@gmail.com>
2025-06-14 23:36:08 +0530

    PS ch2 Q5

--- life14.java
+++ life14.java
@@ -3,7 +3,7 @@ public class life14 {
     public static void main(String[] args) {
         int x = 7;
         int a = (((7*49)/7)+ (35/7));
-        System.out.println("Value of a: "+a);
+        System.out.println( "Value of a: " +a);
 
     }
 }
