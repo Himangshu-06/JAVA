cb85c48f80b280667e44c9a94b999076a83e8c2b
Himangshu-06 <keothimangshu@gmail.com>
2025-06-14 23:33:06 +0530

    Basic concept of String

--- life15.java
+++ life15.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class life15 {
     public static void main(String[] args) {
-        String a = 
+//        String a = "world";
+//        System.out.println("Hello "+a);
+        int a = 3;
+        float b = 5.055678f;
+        System.out.printf("%d is integer and %9.2f is a decimal number\n", a, b);
+        System.out.format("%d is a integer and %f is a decimal number", a, b);
+//        Scanner obj = new Scanner(System.in);
+//        String a = obj.nextLine();
+//        System.out.println(a+ " World");
     }
 }
