bdd0077efd85769489aae2308b8f37a32b23421e
Himangshu <keothimangshu@gmail.com>
2025-06-20 23:50:46 +0530

    PS ch3 Q4

---
+++ life20.java
@@ -0,0 +1,8 @@
+// Write a program to detect double and triple spaces in a string:-
+public class life20 {
+    public static void main(String[] args) {
+        String a = "Hi  m  ang   s   h   u";
+        System.out.println(a.indexOf("  "));
+        System.out.println(a.indexOf("   "));
+    }
+}
