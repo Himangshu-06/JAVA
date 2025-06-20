28bddd84ccd039a62b84ee471728f2852e28b9ed
Himangshu <keothimangshu@gmail.com>
2025-06-20 20:38:45 +0530

    PS ch3 Q1

--- life17.java
+++ life17.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
-public class life17 {
+// Write a Java program to Convert a Sting to lowercase.
+public class life17{
+    public static void main(String[] args) {
+        String a = "HIMANGSHu";
+        System.out.println(a.toLowerCase());
+    }
 }
