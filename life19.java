4345dc058940e39f2970e6c25e51fbcd383881c7
Himangshu <keothimangshu@gmail.com>
2025-06-20 21:38:15 +0530

    PS ch3 Q3

---
+++ life19.java
@@ -0,0 +1,16 @@
+public class life19 {
+    public static void main(String[] args) {
+        String a = "Himangshu";
+        System.out.println("\"Dear "+a+", Thanks a lot\"");
+
+        // ^_^ Other ways to do this task:-
+
+        //1. Using String.replace() method:-
+        String b = "\"Dear |<name>|, Thanks a lot\" ";
+        System.out.println(b.replace("|<name>|","Himangshu"));
+
+        //2.  Using printf()
+        String c= "Himangshu";
+        System.out.printf("\"Dear %s, Thanks a lot\"",c);
+    }
+}
