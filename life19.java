2e337d8e327b041b750b972fd39de5b9cd45686c
Himangshu <keothimangshu@gmail.com>
2025-06-20 23:54:50 +0530

    PS ch3 Q3

--- life19.java
+++ life19.java
@@ -1,3 +1,5 @@
+/* Write a Java program to fill in in a letter template which looks like below: letter ="Dear <|name|>, Thanks a lot"
+Replace <|name|> with a string (some name):- */
 public class life19 {
     public static void main(String[] args) {
         String a = "Himangshu";
