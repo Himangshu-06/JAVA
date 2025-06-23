b1c458874e3a7963dc759e18d5f9b605a9df90a9
Himangshu <keothimangshu@gmail.com>
2025-06-23 12:24:39 +0530

    PS_ch3_Q2

--- life18.java
+++ life18.java
@@ -1,4 +1,4 @@
-// Write a program with replace spaces with underscors:-
+// PS_ch3_Q2 Write a program with replace spaces with underscors:-
 public class life18 {
     public static void main(String[] args) {
         String a = "H i m a n g s h u";
