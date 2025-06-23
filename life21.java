63c3e4d220ecf6ea1c401471113dfc283844edf8
Himangshu <keothimangshu@gmail.com>
2025-06-23 12:38:58 +0530

    PS_ch3_Q5

---
+++ life21.java
@@ -0,0 +1,7 @@
+/* Write a program to format the following latter using escape Sequence Characters:-
+latter = "Dear Harry, This java course is nice, Thank you"*/
+public class life21 {
+    public static void main(String[] args) {
+        System.out.println("\"Dear Harry,\n\tThis java course is nice. \n\t\t Thank you☺️\"");
+    }
+}
