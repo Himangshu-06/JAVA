461a42383c488361ad39d613fcdf58cc1d28d0b6
Himangshu <keothimangshu@gmail.com>
2025-06-25 23:13:12 +0530

    I don't know what I'm doing but I'm trying to make something

--- life23.java
+++ life23.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class life23 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int a = obj.nextInt();
+        System.out.print("Enter a number: ");
+        int b = obj.nextInt();
+        int c = 9;
+        boolean d = (c>10);
+        System.out.println(d);
+
+//        if (a > 10) {
+//            System.out.println("true");
+//        } else {
+//            System.out.println("False");
+//
+//            if (b > 10) {
+//                System.out.println("true");
+//            } else {
+//                System.out.println("False");
+//            }
+//        }
+    }
 }
