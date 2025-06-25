ecffe39831a459ca5cec801c698dd22fb3d3a1cc
Himangshu <keothimangshu@gmail.com>
2025-06-25 12:15:21 +0530

    If-else Statement

--- life22.java
+++ life22.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class life22 {
     public static void main(String[] args) {
-
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = obj.nextInt();
+        if (age>=18)
+        {
+            System.out.println("You can vote");
+        }
+        else{
+            System.out.println("You can't vote");
+        }
     }
 }
