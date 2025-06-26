b72d03826c1bf1b9bba8651e6a64406e0e058137
Himangshu <keothimangshu@gmail.com>
2025-06-26 20:37:43 +0530

    Logical operators

--- life23.java
+++ life23.java
@@ -1,5 +1,7 @@
 // Demonstration of logical operators:-
+
 import java.util.Scanner;
+
 public class life23 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
@@ -12,7 +14,7 @@ public class life23 {
         boolean d = (b > 10); // true if n2 > 10, else false
 
 
-         // Long method(not recommended)
+        // Long method(not recommended)
 //        boolean c,d;
 //        if (a > 10) {
 //             c = true;
@@ -37,25 +39,22 @@ public class life23 {
         }
 
 //        Logical OR:- Evaluates to true if at least one of the conditions is true
-        if(c||d) {
+        if (c || d) {
             System.out.println("At least one number is greater than 10");
-        }
-        else{
+        } else {
             System.out.println("Both numbers are less than or equal to 10");
         }
 
 //         Logical NOT:- Negates the given logic. Basically, if the condition is false, the operation returns true
 //         and when the condition is true, the operation returns false.
-        if(!c) {
+        if (!c) {
             System.out.println("n1 is less than or equal to 10");
-        }
-        else {
+        } else {
             System.out.println("n1 is greater than 10");
         }
-        if(!d){
+        if (!d) {
             System.out.println("n2 is less than or equal to 10");
-        }
-        else {
+        } else {
             System.out.println("n2 is greater than 10");
         }
     }
