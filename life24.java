3d125abc2f5097bd469e60e34a32faf916c63aae
Himangshu <keothimangshu@gmail.com>
2025-06-26 20:57:40 +0530

    If else if ladder

--- life24.java
+++ life24.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+// If else if ladder,
+
+import java.util.Scanner;
 
 public class life24 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter your marks: ");
+        int a = obj.nextInt();
+        if (a >= 90) {
+            System.out.println("Excellent😁");
+        } else if (a >= 80) {
+            System.out.println("Well done☺️");
+        } else if (a >= 70) {
+            System.out.println("Very Good😉");
+        } else if (a >= 60) {
+            System.out.println("Good😏");
+        } else if (a >= 50) {
+            System.out.println("Nice😌");
+        } else if (a >= 40) {
+            System.out.println("Need improvement");
+        } else if (a >= 30) {
+            System.out.println("Barely pass");
+        } else{
+            System.out.println("Better luck next time");
+        }
+
+
+
+    }
 }
