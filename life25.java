a031cf5aa51f3024fd18487f381d1d0b6c637d05
Himangshu <keothimangshu@gmail.com>
2025-07-03 16:59:09 +0530

    not sure

--- life25.java
+++ life25.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
+//vid 18
+import java.util.Scanner;
 public class life25 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        switch (a % 2) {
+            case (0): {
+                System.out.println("Even");
+                break;
+            }
+            default: {
+                System.out.println("odd");
+            }
+
+            //  Alternatively, you can use case (1) [shown below], but keep in mind it doesn't handle negative numbers.
+            case (1): {
+                System.out.println("odd");
+            }
+        }
+    }
 }
