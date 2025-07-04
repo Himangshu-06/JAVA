96568dbb8b7418791d8be79ea67f75736e2674f3
Himangshu <keothimangshu@gmail.com>
2025-07-04 22:29:41 +0530

    practice

--- life26.java
+++ life26.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class life26 {
+    public static void main(String[] args) {
+        String a = "Himangshu";
+        switch (a){
+            case "hi" -> System.out.println("ME");
+            case "Hello"-> System.out.println("YOU");
+            case "Himangshu"-> System.out.println("THEY");
+        }
+    }
 }
