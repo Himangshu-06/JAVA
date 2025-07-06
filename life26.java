80403d6609ad249888081159a71b21611e19028c
Himangshu <keothimangshu@gmail.com>
2025-07-06 21:33:54 +0530

    PS_ch4_Q1

--- life26.java
+++ life26.java
@@ -1,10 +1,14 @@
+//PS_ch4_Q1
+// What will be the output of this program ?
+// Vid 19
 public class life26 {
     public static void main(String[] args) {
-        String a = "Himangshu";
-        switch (a){
-            case "hi" -> System.out.println("ME");
-            case "Hello"-> System.out.println("YOU");
-            case "Himangshu"-> System.out.println("THEY");
+        int a = 10;
+        if(a==11){
+            System.out.println("I am 11");
+        }
+        else {
+            System.out.println("I am not 11");
         }
     }
 }
