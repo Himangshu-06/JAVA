2607302df1031e602a1b9b111f842a7e7dd4e73e
Himangshu <keothimangshu@gmail.com>
2025-07-07 23:45:47 +0530

    test

--- life27.java
+++ life27.java
@@ -3,31 +3,21 @@ public class life27 {
     public static void main(String[] args) {
        Scanner obj = new Scanner(System.in);
         System.out.print("Enter the marks of first subject: ");
-        float a = obj.nextInt();
+        float a = obj.nextFloat();
         System.out.print("Enter the marks of second subject: ");
-        float b = obj.nextInt();
+        float b = obj.nextFloat();
         System.out.print("Enter the marks of third subject: ");
-        float c = obj.nextInt();
+        float c = obj.nextFloat();
         obj.close();
 
-        float d =((a/100)*100);
-        float e =((b/100)*100);
-        float f =((c/100)*100);
+        int totalmarks = 300;
+        float d = ((a+b+c)/totalmarks)*100;
 
-        if (d>=33 && e>=33 && f>=33){
-            System.out.println("You passed");
+        if (a>=33 && b>=33 && c>=33 && d>=40){
+            System.out.printf("You passed. (Your total percentage is= %.2f) \n" ,d);
         }
         else {
-            System.out.println("You failed");
-        }
-
-        float g = (d+e+f);
-
-        if(g>=40){
-            System.out.println("You passed");
-        }
-        else{
-            System.out.println("You failed");
+            System.out.println("You failed(your percentage= "+d+", Better luck next time)");
         }
     }
 }
