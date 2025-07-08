4338ed82471e1574abd08daab7ca64bccdf35f9b
Himangshu <keothimangshu@gmail.com>
2025-07-08 11:47:38 +0530

    PS_ch4_Q2

--- life27.java
+++ life27.java
@@ -1,3 +1,8 @@
+// PS_ch4_Q2
+/*Write a program to find out whether a Student is pass or fail, If it
+requires total 40% and at least 33% in each subject to pass.
+Assume 3 subjects and take marks as an input from the user*/
+// Vid 19
 import java.util.Scanner;
 public class life27 {
     public static void main(String[] args) {
@@ -14,10 +19,10 @@ public class life27 {
         float d = ((a+b+c)/totalmarks)*100;
 
         if (a>=33 && b>=33 && c>=33 && d>=40){
-            System.out.printf("You passed. (Your total percentage is= %.2f) \n" ,d);
+            System.out.printf("You passed. (Your total percentage is= %.2f%%)\n" ,d);
         }
         else {
-            System.out.println("You failed(your percentage= "+d+", Better luck next time)");
+            System.out.printf("You failed(your percentage is= %.2f%% Better luck next time)\n",d);
         }
     }
 }
