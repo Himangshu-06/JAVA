6de78ffc5da6671ecba3c9599f9130da3f50bdff
Himangshu <keothimangshu@gmail.com>
2025-07-08 11:48:53 +0530

    PS_ch4_Q2

--- life27.java
+++ life27.java
@@ -3,10 +3,12 @@
 requires total 40% and at least 33% in each subject to pass.
 Assume 3 subjects and take marks as an input from the user*/
 // Vid 19
+
 import java.util.Scanner;
+
 public class life27 {
     public static void main(String[] args) {
-       Scanner obj = new Scanner(System.in);
+        Scanner obj = new Scanner(System.in);
         System.out.print("Enter the marks of first subject: ");
         float a = obj.nextFloat();
         System.out.print("Enter the marks of second subject: ");
@@ -16,13 +18,12 @@ public class life27 {
         obj.close();
 
         int totalmarks = 300;
-        float d = ((a+b+c)/totalmarks)*100;
+        float d = ((a + b + c) / totalmarks) * 100;
 
-        if (a>=33 && b>=33 && c>=33 && d>=40){
-            System.out.printf("You passed. (Your total percentage is= %.2f%%)\n" ,d);
-        }
-        else {
-            System.out.printf("You failed(your percentage is= %.2f%% Better luck next time)\n",d);
+        if (a >= 33 && b >= 33 && c >= 33 && d >= 40) {
+            System.out.printf("You passed. (Your total percentage is= %.2f%%)\n", d);
+        } else {
+            System.out.printf("You failed(your percentage is= %.2f%% Better luck next time)\n", d);
         }
     }
 }
