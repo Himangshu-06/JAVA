ba5bb61e5c5d2e88c35e1557cd8b7b25e593fbd2
Himangshu <keothimangshu@gmail.com>
2025-07-20 12:08:27 +0530

    test

--- life28.java
+++ life28.java
@@ -4,15 +4,7 @@ public class life28 {
         Scanner obj = new Scanner(System.in);
         System.out.print("Enter your salary(IN LAKHS): ");
         Float a = obj.nextFloat();
-        if(a<2.5){
-            System.out.println("You don't have to pay any tax");
-        }
-        else if(a>=2.5&& a<=5.0){
-            System.out.println("You have to pay 5% tax");
-        }else if(a>=5.0&& a<=10.0){
-            System.out.println("You have to pay 20% tax");
-        }else if(a>=10.0){
-            System.out.println("You have to pay 30% tax");
+
         }
     }
 }
