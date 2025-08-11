e473f94aad4d60be88133693a04027d86d63db4d
Himangshu <keothimangshu@gmail.com>
2025-08-11 17:43:41 +0530

    test

---
+++ life29.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class life29 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter you income(in lakhs): ");
+        float a = obj.nextFloat();
+
+    }
+}
