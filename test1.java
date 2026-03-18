9c42cbebcc43cd3aaf5b26438fe0d17c5740e0d5
Himangshu <keothimangshu@gmail.com>
2026-03-18 20:01:43 +0530

    Test

--- test1.java
+++ test1.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
 public class test1 {
-    public static void main (String[] args){
-        Scanner obj= new Scanner((System.in));
-        System.out.println("Enter 1st subject marks: ");
-        int first = obj.nextInt();
-        System.out.println("Enter 2nd subject name: ");
-        int second = obj.nextInt();
-        System.out.println("Print 3rd subject name: ");
-        int third = obj.nextInt();
+        public static void main(String[] args){
+            Scanner obj = new Scanner(System.in);
+            int day= obj.nextInt();
+            switch (day){
+                case 1:
+                    System.out.println("Sunday");
+                    break;
+                case 2:
+                    System.out.println("Monday");
+                    break;
+                default:
+                    System.out.println("Write a valid number");
+            }
 
-        float percentage= (float) (first + second + third)/ 3.00f;
-        if(first>=33 && second>=33 && third>=33 && percentage>=40){
-            System.out.println("You passed");
         }
-        else{
-            System.out.println("You failed");
-        }
-    }
 }
