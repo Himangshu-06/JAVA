ca55fc3e166b02d0e73b387c6aff6188483c08f5
Himangshu <keothimangshu@gmail.com>
2026-03-19 23:02:12 +0530

    PS_Ch4_Q6

--- life30.java
+++ life30.java
@@ -1,13 +1,16 @@
+/*PS_Ch4_Q6*/
 import java.util.Scanner;
 public class life30 {
     public static void main(String[] args){
         Scanner obj= new Scanner(System.in);
         System.out.print("Enter the domain: ");
         String domain = obj.nextLine();
-        switch (domain){
-            case".com" ->System.out.println("Commercial website");
-            case ".org"-> System.out.println("Organisation website");
-            case ".in"-> System.out.println("Indian website");
+        if(domain.endsWith(".com")){
+            System.out.println("Commercial website");
+        } else if(domain.endsWith(".org")){
+            System.out.println("Organization website");
+        } else if (domain.endsWith(".in")) {
+            System.out.println("Indian website");
         }
     }
 }
