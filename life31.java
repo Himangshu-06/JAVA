53f51984167db5edc9bf1dc854bdf671eb595ea8
Himangshu <keothimangshu@gmail.com>
2026-03-23 15:16:17 +0530

    .

--- life31.java
+++ life31.java
@@ -6,7 +6,14 @@ public class life31 {
         Random number = new Random();
         int random = number.nextInt(4);
         System.out.println("Pick one option: 1.Rock or 2.Paper or 3.Scissors");
-        int side= obj.nextInt();
+        int user= obj.nextInt();
+        if(user==1){
+            System.out.println("You choose: Rock");
+        } else if (user==2) {
+            System.out.println("You choose: Paper");
+        } else if (user==3) {
+            System.out.println("You choose Scissors");
+        }
         if (random == 1) {
             System.out.println("Rock");
         } else if (random == 2) {
@@ -14,14 +21,17 @@ public class life31 {
         } else if (random == 3) {
             System.out.println("Scissors");
         }
-        if(side==random){
+        System.out.println("Computer choose:");
+        if(user==random){
             System.out.println("Tie");
-        } else if (side==1 && random==) {
+        } else if (user==1 && random==3) {
             System.out.println("User win");
-        } else if (side==2) {
+        } else if (user==2 && random==1) {
             System.out.println("User win");
-        } else if (side==3) {
+        } else if (user==3 && random==2) {
             System.out.println("User win");
+        }else {
+            System.out.println("Computer Wins");
         }
 
     }
