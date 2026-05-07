aaab9ef36af7a470c73baa559c2ea6f7d15c976a
Himangshu <keothimangshu@gmail.com>
2026-05-07 17:51:43 +0530

    Loops

--- life32.java
+++ life32.java
@@ -2,11 +2,19 @@ import java.util.Scanner;
 public class life32 {
     public static void main(String[] args){
         Scanner obj = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int age = obj.nextInt();
-        while (age>18){
-            System.out.println("Yo");
-            break;
-        }
+
+//        While Loop:
+//        int i  = 99;
+//        while(i<200){
+//            i++;
+//            System.out.println(i);
+//        }
+
+//        Do-While Loop:
+        int b = 1;
+        int n = 10;
+        do{
+            System.out.println(b++);
+        }while (b>n);
     }
 }
