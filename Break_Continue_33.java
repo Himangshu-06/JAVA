09f35b6e4d3bdd933449eb842071427db3663f70
Himangshu <keothimangshu@gmail.com>
2026-05-10 23:28:08 +0530

    Break and Continue statement

--- Break_Continue_33.java
+++ Break_Continue_33.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Break_Continue_33 {
+    public static void main(String[] args){
+
+        for(int a=0;a<10;a++){
+            System.out.println(a);
+            if(a==3){
+                break;
+            }
+        }
+
+//        for(int a=0;a<10;a++){
+//            System.out.println(a);
+//            if(a==3){
+//                continue;
+//            }
+//            System.out.println("Hello world");
+//        }
+    }
 }
