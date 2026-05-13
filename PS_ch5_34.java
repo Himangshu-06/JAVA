d0617ccf793a0cfb2b2d4e8c1f954a08ddcf703b
Himangshu <keothimangshu@gmail.com>
2026-05-13 21:58:01 +0530

    first question

--- PS_ch5_34.java
+++ PS_ch5_34.java
@@ -1,5 +1,17 @@
 public class PS_ch5_34 {
     public static void main(String[] args){
 
+//        Q1. To print the following pattern: ****
+//                                            ***
+//                                            **
+//                                            *
+
+        for(int i=0;i<=4;i++){
+                System.out.println("*\n");
+                for (int b=3;b<=0;b--){
+                    System.out.println("*");
+                }
+        }
+
     }
 }
