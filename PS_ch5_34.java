fde06556d51c07976c7bf94d7498d383aa0ff8be
Himangshu <keothimangshu@gmail.com>
2026-05-13 23:46:46 +0530

    PS_ch5

--- PS_ch5_34.java
+++ PS_ch5_34.java
@@ -6,12 +6,11 @@ public class PS_ch5_34 {
 //                                            **
 //                                            *
 
-        for(int i=0;i<=4;i++){
-                System.out.println("*\n");
-                for (int b=3;b<=0;b--){
+        for(int i=4;i>=0;i--){
+                System.out.println("*");
+                for (int b=1;b>=i;b--){
                     System.out.println("*");
                 }
         }
-
     }
 }
