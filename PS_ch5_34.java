eecce9067b20ce9fa043fac42eb4c1f461915d62
Himangshu <keothimangshu@gmail.com>
2026-05-16 20:28:39 +0530

    Trying Q1

--- PS_ch5_34.java
+++ PS_ch5_34.java
@@ -6,12 +6,40 @@ public class PS_ch5_34 {
 //                                            **
 //                                            *
 
-        for (int i = 4; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+//        for (int i = 4; i >= 1; i--) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//        Q2. sum first n even numbers using while loop
+
+        int i=0;
+        int no = 5;
+        int j = 0;
+        while(j>no){
+            int even = 2*j;
+            j++;
+            int add = even+j;
+            System.out.println(add);
         }
 
+//       int a=10;
+//       int b=a%2;
+//        while (b!=0){
+//            a+=2;
+//            System.out.println(a);
+//        }
+
+//        int i = 0;
+//        int n=5;
+//        int sum=0;
+//        while(i<n){
+//            i++;
+//            sum=sum+(2*i);
+//        }
+//        System.out.print("Sum of even numbers: ");
+//        System.out.print(sum);
     }
 }
