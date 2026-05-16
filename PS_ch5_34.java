f1b96df5038e656c83fd5026390896614d074231
Himangshu <keothimangshu@gmail.com>
2026-05-16 22:34:09 +0530

    test

--- PS_ch5_34.java
+++ PS_ch5_34.java
@@ -15,15 +15,15 @@ public class PS_ch5_34 {
 
 //        Q2. sum first n even numbers using while loop
 
-        int no = 5;
-        int j = 0;
-        while(j<no){
-            j++;
-            int even = 2*j;
-
-            int add = even+no;
-            System.out.println(add);
-        }
+//        int no = 5;
+//        int j = 0;
+//        while(j<no){
+//            j++;
+//            int even = 2*j;
+//
+//            int add = even+no;
+//            System.out.println(add);
+//        }
 
 //       int a=10;
 //       int b=a%2;
@@ -41,5 +41,35 @@ public class PS_ch5_34 {
 //        }
 //        System.out.print("Sum of even numbers: ");
 //        System.out.print(sum);
+
+//        Q2. Sum first n even numbers using while loop
+
+
+//        Q3. Multiplication table for a given number n
+
+//        int n=5;
+//        int c=0;
+//        for (int a=0;a<=10;a++){
+//            int b= c*n;
+//            c++;
+//            System.out.println(b);
+//        }
+
+//        Q4. Multiplication table of 10 in reverse order
+//        int n =9;
+//        for (int x= 10;x>=1;x--){
+//            int y = n *x;
+//            System.out.println(y);
+//        }
+
+//        Q5. Factorial of a given number using for loop
+
+        int n = 4;
+        int c=1;
+        for (int a =1;a<=n;a++){
+            c++;
+            int b =a*c;
+            System.out.println(b);
+        }
     }
 }
