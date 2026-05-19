7581233b61bd672d8439f38f82f62c07d50c09bd
Himangshu <keothimangshu@gmail.com>
2026-05-19 21:01:20 +0530

    Arrays

--- Array_35.java
+++ Array_35.java
@@ -2,14 +2,29 @@ public class Array_35 {
     public static void main(String[] args){
    /*3*/     int[] marks = {10,20,30,40,50};
         marks[2]=80; //overwriting
-        System.out.println(marks[2]);
+//        System.out.println(marks[2]);
 
 //         There are three main ways to create an array in Java
 //        1.  int [] marks= new int[10];
 //        2.  int [] marks;
 //        marks=new int[5];
+        String[] hello= {"Hello", "Hi", "Bonjour"};
+        System.out.println(hello[1]);
 
+//        To displaying the array
+//        for (int a=0;a<marks.length;a++){
+//            System.out.println(marks[a]);
+//        }
 
+//        Quick Quiz - Array in reverse order ?
+//        for (int b=marks.length-1;b>=0;b--){
+//            System.out.println(marks[b]);
+//        }
+
+//        Displaying using for-each loop
+        for (int var: marks){
+            System.out.println(var);
+        }
 
     }
 }
---
+++ Multidimensional_Array_36.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class Multidimensional_Array_36 {
+}
