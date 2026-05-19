3a7609b19094add72eecdb4a8b748162473b9c60
Himangshu <keothimangshu@gmail.com>
2026-05-19 13:38:00 +0530

    Array

--- Array_35.java
+++ Array_35.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Array_35 {
+    public static void main(String[] args){
+//        int [] marks= new int[10];
+        int[] marks = {10,20,30,40,50};
+        marks[2]=80; //overwriting
+        System.out.println(marks[2]);
+    }
 }
