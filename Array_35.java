470c2f21fbd429971a611a98f77a6c627493ad66
Himangshu <keothimangshu@gmail.com>
2026-05-19 14:13:53 +0530

    Array creation

--- Array_35.java
+++ Array_35.java
@@ -1,8 +1,15 @@
 public class Array_35 {
     public static void main(String[] args){
-//        int [] marks= new int[10];
-        int[] marks = {10,20,30,40,50};
+   /*3*/     int[] marks = {10,20,30,40,50};
         marks[2]=80; //overwriting
         System.out.println(marks[2]);
+
+//         There are three main ways to create an array in Java
+//        1.  int [] marks= new int[10];
+//        2.  int [] marks;
+//        marks=new int[5];
+
+
+
     }
 }
