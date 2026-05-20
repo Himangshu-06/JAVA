09f8ed8f8d317f257f103b450693c0958bee5b6c
Himangshu <keothimangshu@gmail.com>
2026-05-20 20:34:12 +0530

    2D Array

--- Multidimensional_Array_36.java
+++ Multidimensional_Array_36.java
@@ -1,7 +1,12 @@
 public class Multidimensional_Array_36 {
     public static void main(String [] args){
         int[] one ={1,2,3}; // 1D Array
-        int[][] two; // 2D Array
-        two = [3][5];
+        int[][] two=  {{1,2,3},{4,5,6},{7,8,9}}; // 2D Array;
+        for (int a=0;a<two.length;a++){
+            System.out.println(a);
+            for (int b=0; b<two[a].length; b++){
+                System.out.print(two[a][b] +" ");
+            }
+        }
     }
 }
