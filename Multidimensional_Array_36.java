8388271f9673f1703526a8379ef6f02fea93b363
Himangshu <keothimangshu@gmail.com>
2026-05-20 12:11:20 +0530

    test

--- Multidimensional_Array_36.java
+++ Multidimensional_Array_36.java
@@ -2,5 +2,6 @@ public class Multidimensional_Array_36 {
     public static void main(String [] args){
         int[] one ={1,2,3}; // 1D Array
         int[][] two; // 2D Array
+        two = [3][5];
     }
 }
