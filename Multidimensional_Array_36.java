b4958b37865874c9e15dcbfaba945afcc8086c5d
Himangshu <keothimangshu@gmail.com>
2026-05-20 23:05:08 +0530

    Multidimensional Array

--- Multidimensional_Array_36.java
+++ Multidimensional_Array_36.java
@@ -1,11 +1,17 @@
 public class Multidimensional_Array_36 {
     public static void main(String [] args){
-        int[] one ={1,2,3}; // 1D Array
-        int[][] two=  {{1,2,3},{4,5,6},{7,8,9}}; // 2D Array;
-        for (int[] num: nums){
-            for (int inner: numbers){
+//         1D Array
+        int[] oneD ={1,2,3};
+
+//         2D Array;
+        int[][] twoD =  {{1,2,3},{4,5,6},{7,8,9}};
+        twoD[0][1]=200; // To Access or replace elements (Hint: It's like Matrix)
+
+        for (int[] outer : twoD){
+            for (int inner: outer){
                 System.out.print(inner +" ");
             }
+            System.out.println();
         }
     }
 }
