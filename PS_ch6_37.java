5689ece37b1a859ba26751ff6f0c2fb5474d7d8c
Himangshu <keothimangshu@gmail.com>
2026-05-21 21:06:19 +0530

    Complete Q2{Not Sure}

--- PS_ch6_37.java
+++ PS_ch6_37.java
@@ -1,18 +1,44 @@
+import java.util.Scanner;
 public class PS_ch6_37 {
     public static void main(String[] args){
-
+        Scanner sc= new Scanner(System.in);
 //        Q1. 5 floats and their sum
 //        float[] a= {0f};
-        float[] var = {1.5f, 2.5f,3.5f,4.5f,5.5f};
+//        float[] arr = {1.5f, 2.5f,3.5f,4.5f,5.5f};
 //        for (float plus : var){
 //            System.out.println(plus);
 //             var=a;
 //            ;
-        float sum = 0f;
-        for (int a=0; a<var.length; a++){
-            sum= sum+var[a];
-            System.out.println(sum);
+//        float sum = 0f;
+//        for (int a=0; a<var.length; a++){
+//            sum= sum+var[a];
+//            System.out.println(sum);
+//        }
 
+//        Or (Using Enhance for/ for each loop)
+//        float sum =0;
+//        for (float var: arr){
+//            sum+=var;
+//            System.out.println(sum);
+
+//            Q2. to find out whether an integer is present in an array or not ?
+        System.out.print("Enter a number to check: ");
+        int b= sc.nextInt();
+        int[] array ={1,2,3,4,5};
+        boolean found = false;
+        for (int var : array){
+
+            if (var==b){
+                found= true;
+            }
         }
+        if (found){
+            System.out.println("The number is present");
+        }else {
+            System.out.println("The number is not present");
+        }
+
+
+
     }
 }
