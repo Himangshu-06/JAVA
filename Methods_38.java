e00d05a965ed07ccb85841cd1a4a831437749125
Himangshu <keothimangshu@gmail.com>
2026-05-25 23:07:01 +0530

    Calling a Method using an Object

--- Methods_38.java
+++ Methods_38.java
@@ -7,10 +7,17 @@ public class Methods_38 {
         return c;
     }
     public static void main(String[] args){
+//        Creating an object to call a Method
+//        Methods_38 obj= new Methods_38();
+//        This works for non-static methods
         int x= 10;
         int y=20;
         int u;
+//         u= obj.sum(x,y);
         u= sum(x,y);
         System.out.println(u);
+
+/*       The values from the method call (a and b) are copied tp the a and b of the function of sum Thus even if we modify the values a and b inside the method, the values in the main method will not change */
+
     }
 }
