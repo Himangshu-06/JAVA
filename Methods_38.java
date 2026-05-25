1d39fa483605329f8c63736c35a2999dfb50a0bd
Himangshu <keothimangshu@gmail.com>
2026-05-25 19:56:34 +0530

    Methods

---
+++ Methods_38.java
@@ -0,0 +1,13 @@
+public class Methods_38 {
+    static int sum(int a, int b){
+        int c;
+        c=a+b;
+        return c;
+    }
+
+    int x= 10;
+    int y=20;
+    public static void main(String[] args){
+
+    }
+}
