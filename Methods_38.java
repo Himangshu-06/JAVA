097be4ec5db04dc30cc60097fbe191e1d619bb23
Himangshu <keothimangshu@gmail.com>
2026-05-25 20:07:06 +0530

    First Method

--- Methods_38.java
+++ Methods_38.java
@@ -1,13 +1,16 @@
+// A Method is a function written inside a class.
+
 public class Methods_38 {
     static int sum(int a, int b){
         int c;
-        c=a+b;
+        c=a*b;
         return c;
     }
-
-    int x= 10;
-    int y=20;
     public static void main(String[] args){
-
+        int x= 10;
+        int y=20;
+        int u;
+        u= sum(x,y);
+        System.out.println(u);
     }
 }
