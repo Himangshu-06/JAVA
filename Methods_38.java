75dbc0890bcf9bf69d801e08d1d8e613ef9e08ed
Himangshu <keothimangshu@gmail.com>
2026-05-25 20:39:39 +0530

    Revert "calling the method using an object"
    This reverts commit 6a50006b9436c7f5f85042849a274c3ece4579c9.

--- Methods_38.java
+++ Methods_38.java
@@ -1,20 +1,16 @@
 // A Method is a function written inside a class.
 
 public class Methods_38 {
-     static int sum(int a, int b){
+    static int sum(int a, int b){
         int c;
         c=a*b;
         return c;
     }
     public static void main(String[] args){
-//         Creating an object to call a Method
-//        Methods_38 obj= new Methods_38();
-//        This works for non-static methods
         int x= 10;
         int y=20;
         int u;
-        u=sum(x,y);
-//        u= obj.sum(x,y);
+        u= sum(x,y);
         System.out.println(u);
     }
 }
