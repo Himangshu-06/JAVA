60ff615da5a255b5f29edf6662dc0a2e2219807c
Himangshu <keothimangshu@gmail.com>
2026-05-26 23:54:55 +0530

    Method Overloading

--- Method_overloading_39.java
+++ Method_overloading_39.java
@@ -20,6 +20,7 @@ public class Method_overloading_39 {
     }
 
 //    Void return type - When we don't want our method to return anything we use void as the return type.
+
     static void sayHello(){
         System.out.println("bonjour mon ami");
     }
