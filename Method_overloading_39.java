d54ac71731f97d72683e97ceb131951a86d89d2f
Himangshu <keothimangshu@gmail.com>
2026-05-26 22:53:05 +0530

    Differentiate between pass by value/reference

--- Method_overloading_39.java
+++ Method_overloading_39.java
@@ -1,10 +1,26 @@
 public class Method_overloading_39 {
+    static void change1(int a){
+        a=100;
+    }
+    static void change2(int[] arr){
+        arr[5]=100;
+    }
 //    Void return type - When we don't want our method to return anything we use void as the return type.
     static void sayHello(){
         System.out.println("bonjour mon ami");
     }
     public static void main(String[] args){
+
+//        Case 1: Pass by value
+        int z =10;
+        change1(z);
+        System.out.println(z);
         sayHello();
+
+//        Pass by reference
+        int[] arr = {1,2,3,4,5,6};
+        change2(arr);
+        System.out.println(arr[5]);
 
         /* * Pass by value — a copy of the actual data is passed to the method. Whatever the method does with it, the original variable remains unaffected.
            * Pass by reference — the memory address of the actual data is passed to the method. The method goes to that location and works on the original data directly, so changes are reflected outside the method too.
