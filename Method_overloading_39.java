af555fe7ac5aac5e6ba5d29829201b53ff7e1450
Himangshu <keothimangshu@gmail.com>
2026-05-26 13:21:00 +0530

    Starting Method overloading

---
+++ Method_overloading_39.java
@@ -0,0 +1,10 @@
+public class Method_overloading_39 {
+    static void hello(){
+        System.out.println("bonjour mon ami");
+    }
+    public static void main(String[] args){
+        hello();
+        System.out.println();
+    }
+
+}
