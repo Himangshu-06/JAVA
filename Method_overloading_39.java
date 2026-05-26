bbcde324c094c980e89951f9b20aefb31217c532
Himangshu <keothimangshu@gmail.com>
2026-05-26 13:35:47 +0530

    Void return type

--- Method_overloading_39.java
+++ Method_overloading_39.java
@@ -1,10 +1,11 @@
 public class Method_overloading_39 {
-    static void hello(){
+//    Void return type - When we don't want our method to return anything we use void as the return type.
+    static void sayhello(){
         System.out.println("bonjour mon ami");
     }
     public static void main(String[] args){
-        hello();
-        System.out.println();
-    }
+        sayhello();
 
+//
+    }
 }
