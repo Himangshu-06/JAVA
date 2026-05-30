b1167b8ccc7b6094d7e673c4a1c8c131636dd336
Himangshu <keothimangshu@gmail.com>
2026-05-30 22:46:18 +0530

    Fibonacci sequence

--- Recursion_40.java
+++ Recursion_40.java
@@ -1,4 +1,6 @@
 public class Recursion_40 {
+//    Find factorial of a number using recursion
+
 //    static int factorial(int a){
 //        if (a==0 || a==1){
 //            return a;
@@ -9,18 +11,33 @@ public class Recursion_40 {
 
 //            Write a recursive function to calculate the sum of the first n natural numbers.
 
-    static int sum(int n){
-        if(n==0){
+//    static int sum(int n){
+//        if(n==0){
+//            return 0;
+//        }else {
+//            return n + sum(n - 1);
+//        }
+//    }
+
+//         Fibonacci series
+
+    static int fibonacci(int a){
+        if (a==0){
             return 0;
+        } if (a==1){
+            return 1;
         }else {
-            return n+ sum(n-1);
-}
+            return fibonacci(a - 1) + fibonacci(a - 2);
+        }
     }
+
     public static void main(String[] args) {
 //        int z = 3;
 //        System.out.printf("The factorial of %d is: %d" ,z ,factorial(z));
 
-        int a = 4;
-        System.out.println("Sum of first "+a+" number is: " + sum(a));
+//        int a = 4;
+//        System.out.println("Sum of first "+a+" number is: " + sum(a));
+
+        System.out.println("Fibonacci number is: " +fibonacci(6));
     }
 }
