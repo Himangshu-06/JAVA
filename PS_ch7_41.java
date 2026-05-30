2cffc3eae7305167ed40a3108545e1c22e90fa09
Himangshu <keothimangshu@gmail.com>
2026-05-30 23:06:31 +0530

    Q1 Method for Multiplication table

--- PS_ch7_41.java
+++ PS_ch7_41.java
@@ -1,9 +1,12 @@
-// Q1.
+//
 public class PS_ch7_41 {
-    static int var(int a, int b){
-        return a+b;
+//    Create a Java method to print multiplication table of number n ?
+    static void multiply(int n) {
+        for (int a = 0; a <= 10; a++) {
+            System.out.printf("%d x %d: %d \n", n, a, n * a);
+        }
     }
     public static void main(String[] args) {
-        System.out.println("Addition: " + var(1 ,2));
+        multiply(3);
     }
 }
