91d0167b72351ce54f82a277760921b45a665207
Himangshu <keothimangshu@gmail.com>
2026-05-31 13:31:00 +0530

    Trying Q2

--- PS_ch7_41.java
+++ PS_ch7_41.java
@@ -1,12 +1,29 @@
-//
+
 public class PS_ch7_41 {
-//    Create a Java method to print multiplication table of number n ?
-    static void multiply(int n) {
-        for (int a = 0; a <= 10; a++) {
-            System.out.printf("%d x %d: %d \n", n, a, n * a);
+//   Q1. Create a Java method to print multiplication table of number n ?
+//    static void multiply(int n) {
+//        for (int a = 0; a <= 10; a++) {
+//            System.out.printf("%d x %d: %d \n", n, a, n * a);
+//        }
+//    }
+
+//        Q2. Function t print the following pattern ****
+//                                                   ***
+//                                                   **
+//                                                   *
+
+    static void star(int a, int b){
+        for (a=0;a>b;a++){
+            for (b=0;b<a;b--){
+                System.out.println("*");
+            }
+            System.out.println();
         }
     }
+
     public static void main(String[] args) {
-        multiply(3);
+//        multiply(3);
+
+        star(4,3);
     }
 }
