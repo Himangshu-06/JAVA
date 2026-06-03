7b7d19169d6a88fa3f7dc09ae95619be4937d1cf
Himangshu <keothimangshu@gmail.com>
2026-06-03 20:54:27 +0530

    Q6 still working

--- PS_ch7_41.java
+++ PS_ch7_41.java
@@ -48,14 +48,22 @@ public class PS_ch7_41 {
 
 //    Q5. Function to print nth term of Fibonacci series using recursion ?
 
-    static int fibonacci(int a){
-        if(a==0) return 0;
-        if(a==1) return 1;
-        return fibonacci(a-1)+fibonacci(a-2);
+//    static int fibonacci(int a){
+//        if(a==0) return 0;
+//        if(a==1) return 1;
+//        return fibonacci(a-1)+fibonacci(a-2);
+//    }
+
+//    Q6. Function to find average of a set of numbers passed as arguments ?
+    static int average(int ...arr){
+        int sum =0;
+        for (int a =0; a<arr.length; a++){
+            if (arr.length == 0) return 0;
+            return (sum+=a)/2;
+
+        }
+
     }
-
-//    Q6. Function to find average of a set of numbers passed as arguments
-
     public static void main(String[] args) {
 
 //        Q1:
@@ -71,6 +79,10 @@ public class PS_ch7_41 {
 //        starInverted(4);
 
 //        Q5:
-        System.out.println("Fibonacci number is: "+fibonacci(7));
+//        System.out.println("Fibonacci number is: "+fibonacci(7));
+
+//        Q6:
+        int[] x ={12,23,34,45,56,67,78};
+
     }
 }
