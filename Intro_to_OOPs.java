6aadd838b4d1bba6898e56e5cb7b3077acc039e6
Himangshu <keothimangshu@gmail.com>
2026-06-07 22:17:55 +0530

    test Classes and objects

--- Intro_to_OOPs.java
+++ Intro_to_OOPs.java
@@ -2,5 +2,7 @@ public class Intro_to_OOPs {
 
     public static void main(String[] args) {
 
+//        class- Classes are like the structure
+//        object- Object is the properties, action of a object
     }
 }
