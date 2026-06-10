5d65f27ddf43d6bb141f2c9e7d8edfada489ef42
Himangshu <keothimangshu@gmail.com>
2026-06-10 21:02:28 +0530

    test

--- Intro_to_OOPs.java
+++ Intro_to_OOPs.java
@@ -2,7 +2,8 @@ public class Intro_to_OOPs {
 
     public static void main(String[] args) {
 
-//        class- Classes are like the structure
-//        object- Object is the properties, action of a object
+//        class - Classes are like the structure
+//        object - Object is the properties, action of an object
+//        class contain an object and an object contain properties, methods...
     }
 }
