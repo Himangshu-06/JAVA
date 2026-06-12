ec0e868005fafca70330076fdd7d3c3b6f9fbef8
Himangshu <keothimangshu@gmail.com>
2026-06-12 23:26:24 +0530

    getter and setter and Access modifires

---
+++ AcessModifires_Constructors_45.java
@@ -0,0 +1,20 @@
+//        Access Modifiers - Specify where a property/method is accessible. There are four types of access modifiers in java :
+//        1. private
+//        2. default
+//        3. protected
+//        4. public
+//        Getter - Method that make a field readable.
+//        Setter - Method that make a field writeable.
+//        They help to protect object data and rules for accessing or modifying them.
+
+class student{
+    private String name;
+    private int roll;
+}
+public class AcessModifires_Constructors_45 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
