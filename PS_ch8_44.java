129b0a480b00c7b9cdc97288c32ac00a9055a4ed
Himangshu <keothimangshu@gmail.com>
2026-06-12 20:56:31 +0530

    Q3

--- PS_ch8_44.java
+++ PS_ch8_44.java
@@ -1,4 +1,4 @@
-/* Q1. Create a class employee with following properties and methods:
+/*      Q1. Create a class employee with following properties and methods:
     salary(property)(int)
     getSalary(method) (returning int)
     name (property)(int)
@@ -20,13 +20,25 @@
 //    }
 //}
 
-//        Q2. Create a class cellphone with methods to print "ringing", "vibrating"...etc.
-class cellphone{
-    public void ring(){
-        System.out.println("Phone is ringing");
+//      Q2. Create a class cellphone with methods to print "ringing", "vibrating"...etc.
+//class cellphone{
+//    public void ring(){
+//        System.out.println("Phone is ringing");
+//    }
+//    public void vibrate(){
+//        System.out.println("Phone is vibrating");
+//    }
+//}
+
+//      Q3. Create a class square with a method to initialize it's side, calculating area, perimeter etc.
+class square{
+    int side;
+
+    public int area(){
+        return side*side;
     }
-    public void vibrate(){
-        System.out.println("Phone is vibrating");
+    public int perimeter(){
+        return 4*side;
     }
 }
 
@@ -42,8 +54,14 @@ public class PS_ch8_44 {
 //        System.out.println("Name: "+identity.getName());
 
 //        Q2.
-        cellphone phone = new cellphone();
-        phone.ring();
-        phone.vibrate();
+//        cellphone phone = new cellphone();
+//        phone.ring();
+//        phone.vibrate();
+
+//        Q3.
+        square calculate = new square();
+        calculate.side = 5;
+        System.out.println("Area of the square is: " +calculate.area());
+        System.out.println("Perimeter of the square is: " +calculate.perimeter());
     }
 }
