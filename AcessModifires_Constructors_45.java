acc93ba23168420d7b69ef34c23af4057220d75f
Himangshu <keothimangshu@gmail.com>
2026-06-13 09:22:00 +0530

    example of getter & setter

--- AcessModifires_Constructors_45.java
+++ AcessModifires_Constructors_45.java
@@ -10,11 +10,28 @@
 class student{
     private String name;
     private int roll;
+
+    public void setName(String a){
+        name =a;
+    }
+    public String  getName(){
+        return name;
+    }
+
+    public void setRoll(int b){
+        roll = b;
+    }
+    public int getRoll(){
+        return roll;
+    }
 }
 public class AcessModifires_Constructors_45 {
     public static void main(String[] args) {
+        student details = new student();
 
-
-
+        details.setName("Himangshu");
+        System.out.println("My name is "+ details.getName());
+        details.setRoll(5);
+        System.out.println("My roll no is "+details.getRoll());
     }
 }
