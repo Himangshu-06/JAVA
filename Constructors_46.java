82ee69a2f2890a9f402223f9e6a185e916154e31
Himangshu <keothimangshu@gmail.com>
2026-06-13 22:35:00 +0530

    Starting constructor

---
+++ Constructors_46.java
@@ -0,0 +1,7 @@
+//  Constructor - A special method to initialize objects.
+//                You can pass arguments to constructor and setup initial value.
+public class Constructors_46 {
+    Constructors_46(){
+
+    }
+}
