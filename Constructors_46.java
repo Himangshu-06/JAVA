f1d0aa2f22635885f86927efc8ffc335ca9a737a
Himangshu <keothimangshu@gmail.com>
2026-06-14 16:46:16 +0530

    Terminologies is constructors

--- Constructors_46.java
+++ Constructors_46.java
@@ -1,7 +1,21 @@
-//  Constructor - A special method to initialize objects.
-//                You can pass arguments to constructor and setup initial value.
-public class Constructors_46 {
-    Constructors_46(){
+ /* Constructor - A constructor is a special method that runs automatically when you create an object. Its job is to set up the initial values of that object.
+      Two things that make it special:
+  It has the same name as the class
+  It has no return type
 
+      Field - A field (also called an instance variable) is a variable that belongs to a class and represents the state of an object. Each object of the class has its own copy of these variables.
+
+      State - The state of an object refers to the current values of its fields (instance variables) at any given time. It describes what the object knows or what data it holds.
+
+      A variable is a field (instance variable) only if:
+  It is declared inside a class but outside any method or constructor.
+  It belongs to each instance (object) of the class.
+  It represents the state of the object.*/
+public class Constructors_46 {
+    String name;
+    int roll;
+    Constructors_46(String name, int roll){
+        this.name = name;
+        this.roll = roll;
     }
 }
