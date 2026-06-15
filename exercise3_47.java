f80736c37343bce4a94e76f9925b29bc7486139b
Himangshu <keothimangshu@gmail.com>
2026-06-15 20:05:39 +0530

    checking logic of Ex.3

---
+++ exercise3_47.java
@@ -0,0 +1,26 @@
+// Q. Create a class Game, which allows a user to play "Guess the Number" game once.
+//  Game should have the following methods:
+//  Constructor to generate the random number
+//  takeUserInput() to take a user input of number
+//  isCorrectNumber() to detect whether the number entered by the user is true
+//  getter and setter for noOfGuesses
+//  Use properties such as noOfGuesses(int), etc to get this task done!
+
+import java.util.Scanner;
+import java.util.Random;
+
+class Guess{
+    private int number;
+    private int sc;
+     Guess(){
+        Random number = new Random();
+         Scanner sc = new Scanner(System.in);
+         if(number==sc)
+     }
+}
+public class exercise3_47 {
+    public static void main() {
+
+//        while (Guess == )
+    }
+}
