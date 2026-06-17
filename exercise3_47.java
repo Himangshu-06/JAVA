0f1547439b2eb531162281828b9af1696512c9cb
Himangshu <keothimangshu@gmail.com>
2026-06-17 21:16:25 +0530

    Finished basic working logic

--- exercise3_47.java
+++ exercise3_47.java
@@ -6,30 +6,41 @@
 //  getter and setter for noOfGuesses
 //  Use properties such as noOfGuesses(int), etc to get this task done!
 
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 class Guess{
     private int randnumber;
     private int takeUserInput;
+    private int noOfAttempt;
     Guess(){
         Random rand = new Random();
         this.randnumber = rand.nextInt(10,20);
-        System.out.println(randnumber);
+    }
+    public int getRandnumber(){
+        return randnumber;
     }
 
     public void TakeUserInput(){
         Scanner sc = new Scanner(System.in);
         this.takeUserInput = sc.nextInt();
-
+        noOfAttempt++;
+    }
+    public int getTakeUserInput(){
+        return this.takeUserInput;
     }
 }
 public class exercise3_47 {
     public static void main(String[] args) {
+        System.out.println("Enter a number between 10 - 20");
         Guess game = new Guess();
 
         do {
-            System.out.println("Enter a guess: ");
-        }while (game != )
+            System.out.print("Enter your lucky no :(again) ");
+            game.TakeUserInput();
+
+        }while (game.getTakeUserInput() != game.getRandnumber());
+        System.out.println("Good job lad");
     }
+
 }
