0f2c2c2d6f18d0244d2ef0e1a2d3228fff99a5e3
Himangshu <keothimangshu@gmail.com>
2026-07-05 22:26:37 +0530

    test(code): run Codex-generated code for better understanding

--- test.java
+++ test.java
@@ -1,52 +1,52 @@
-import java.util.Random;
-import java.util.Scanner;
-
-class Guess_game{
-    private int comp_num;
-    private int user_num;
-    private int chances;
-    Guess_game(){
-        System.out.println("Welcome to number guess game: ");
-        Random r=new Random();
-        comp_num=r.nextInt(100);  // 0 to 99
-        chances=0;
-
-    }
-    void user_input(int x){
-        user_num=x;
-    }
-    void correct_number(){
-        if(user_num>comp_num){
-            System.out.println("you slected a big number: ");
-            chances++;
-        }
-        else if(user_num<comp_num){
-            System.out.println("you slected a small number: ");
-            chances++;
-        }
-        else{
-            System.out.println("you got it right!!!");
-            chances++;
-            System.out.println("nNo of chances you taken: "+chances);
-
-        }
-    }
-
-}
-
-public class test {
-    public static void main(String[] args) {
-        Guess_game player1=new Guess_game();
-        while(true){
-            int n;
-            Scanner sc=new Scanner(System.in);
-            System.out.println("Enter the user number : ");
-            n=sc.nextInt();
-            player1.user_input(n);
-            player1.correct_number();
-        }
-
-    }
-
-
-}
+//class Animal {
+//    private int noOfLegs;
+//    private boolean canFly;
+//
+//    public int getNoOfLegs() {
+//        return noOfLegs;
+//    }
+//
+//    public void setNoOfLegs(int noOfLegs) {
+//        this.noOfLegs = noOfLegs;
+//    }
+//
+//    public boolean getCanFly() {
+//        return canFly;
+//    }
+//
+//    public void setCanFly(boolean canFly) {
+//        this.canFly = canFly;
+//    }
+//}
+//
+//class Dog extends Animal {
+//    private boolean bark;
+//    private boolean walk;
+//
+//    public boolean isBark() {
+//        return bark;
+//    }
+//
+//    public void setBark(boolean bark) {
+//        this.bark = bark;
+//    }
+//
+//    public boolean isWalk() {
+//        return walk;
+//    }
+//
+//    public void setWalk(boolean walk) {
+//        this.walk = walk;
+//    }
+//}
+//
+//public class Inheritance_49 {
+//    public static void main(String[] args) {
+//        Dog attri = new Dog();
+//        attri.setNoOfLegs(4);
+//        attri.setCanFly(false);
+//
+//        System.out.println("No of legs: " + attri.getNoOfLegs());
+//        System.out.println("Can fly? " + attri.getCanFly());
+//    }
+//}
