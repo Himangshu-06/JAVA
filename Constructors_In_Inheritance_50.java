455984417bf9c0ef9083aecddf94b8f273f68275
Himangshu <keothimangshu@gmail.com>
2026-07-06 22:04:32 +0530

    practice(oop): add getters, setters, and constructors for Species and Human classes

--- Constructors_In_Inheritance_50.java
+++ Constructors_In_Inheritance_50.java
@@ -1,3 +1,44 @@
+class Species{
+    private int legs;
+    private int hands;
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public void setLegs(int legs) {
+        this.legs = legs;
+    }
+
+    public int getHands() {
+        return hands;
+    }
+
+    public void setHands(int hands) {
+        this.hands = hands;
+    }
+}
+
+class Human extends Species{
+    private int speak;
+    private int walk;
+
+    public int getSpeak() {
+        return speak;
+    }
+
+    public void setSpeak(int speak) {
+        this.speak = speak;
+    }
+
+    public int getWalk() {
+        return walk;
+    }
+
+    public void setWalk(int walk) {
+        this.walk = walk;
+    }
+}
 public class Constructors_In_Inheritance_50 {
     public static void main(String[] args) {
 
