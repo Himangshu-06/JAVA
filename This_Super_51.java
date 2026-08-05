cb0117b3937b5b923c7d23fd2e2c49f0edb7df2a
Himangshu <keothimangshu@gmail.com>
2026-08-05 23:38:08 +0530

    docs(notes): add study notes from CwH in previous topic and organize them

--- This_Super_51.java
+++ This_Super_51.java
@@ -5,7 +5,7 @@ class Class1 extends Object{
 public class This_Super_51 {
     public static void main(String[] args) {
 
-        /* Notes -
+        /* Notes from Telusko-
 #1
 Whenever you call an object, it will create a constructor of a class.
 When we create an object of a class that it calls the constructor of a sub-class as well as a constructor of a superclass.
@@ -35,6 +35,18 @@ If you pass any parameter in this method, then it will call the parameterised co
 We can use both this and super method anywhere except the static area.
 Both this and super method can be used any number of times in a code.
 But both can be used only one time inside a constructor.
-Both are non-static keywords.*/
+Both are non-static keywords.
+
+        Notes from CwH -
+#1 "this" keyword in Java :
+this is a way for us to reference an object of the class which is being created/referenced.
+It is used to call the default constructor of the same class.
+this keyword eliminates the confusion between the parameters and the class attributes with the same name.
+
+#2 Super keyword
+A reference variable used to refer immediate parent class object.
+It can be used to refer immediate parent class instance variable.
+It can be used to invoke the parent class method.
+*/
     }
 }
