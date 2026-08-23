ababfa704d04a6ec5d1159cdfbcc20f40342a5f9
Himangshu <keothimangshu@gmail.com>
2026-08-23 20:25:07 +0530

    feat(java): extend Fish from Bird superclass and implement method override

--- Method_overriding_52.java
+++ Method_overriding_52.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+class Bird{
 
+    public void legs(){
+        System.out.println("A Bird has two legs");
+    }
+    public void eye(){
+        System.out.println("A Bird has two eyes");
+    }
+}
+class Fish extends Bird{
+    @Override
+    public void legs(){
+        System.out.println("A Fish doesn't have legs");
+    }
+}
 public class Method_overriding_52 {
+    public static void main(String[] args){
+        Bird a = new Bird();
+        a.legs();
+
+        Fish b = new Fish();
+        b.legs();
+    }
 }
