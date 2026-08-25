8160115226148a2852c7a940e37dd75fd2d21e75
Himangshu <keothimangshu@gmail.com>
2026-08-25 20:47:02 +0530

    feat(java): implement dynamic method dispatch

--- Dynamic_dispatch_53.java
+++ Dynamic_dispatch_53.java
@@ -1,24 +1,23 @@
+
 class Car{
 
     public void OilCar(){
         System.out.println("A Oil powered car run by an combustion Engine");
     }
     public void electricCar(){
-        System.out.println("An Electric vehicle runs on battery and motor");
+        System.out.println(" 1 An Electric vehicle runs on battery and motor");
     }
 }
 class Tesla extends Car{
     @Override
     public void electricCar(){
-        System.out.println("Electric vehicle runs on battery so it do low carbon emission");
+        System.out.println(" 2 Electric vehicle runs on battery so it do low carbon emission");
     }
 }
 public class Dynamic_dispatch_53{
     public static void main(String[] args){
-        Bird a = new Bird();
-        a.legs();
+        Car a = new Tesla();
+        a.electricCar();
 
-        Fish b = new Fish();
-        b.legs();
     }
 }
