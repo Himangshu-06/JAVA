95cd53dc83dce2dca56391ef647099cfb22d7c0d
Himangshu <keothimangshu@gmail.com>
2026-08-27 20:05:18 +0530

    feat(java): attempt practice set Q1 by extending a class

--- PS_ch10_54.java
+++ PS_ch10_54.java
@@ -1,5 +1,11 @@
 
-// Q1.  Create a class circle and use inheritance to create another class cylinder from it.
+// Q1.  Create a class circle and use inheritance to create another class cylinder from it ?
+class  Circle{
+
+}
+class cylinder extends Circle{
+
+}
 public class PS_ch10_54 {
     public static void main (String[] args){
 
