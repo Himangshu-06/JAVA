8e74471c69cee57d39850461ef427b7514acb78b
Himangshu <keothimangshu@gmail.com>
2026-08-29 12:28:47 +0530

    feat(java): create Library class with addBook method

--- Exercise4_54.java
+++ Exercise4_54.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+/* Q: You have to implement a library using Java Class "Library"
+Methods: addBook, issueBook, returnBook, showAvailableBooks
+Properties: Array to store the available books, Array to store the issued books */
 
+import java.util.Scanner;
+class Library{
+    int[] availableSlots = {100};
+    int count = 0;
+    public void addBook(){
+        count++;
+    }
+}
 public class Exercise4_54 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the unique ID of the book: ");
+        int unique = obj.nextInt();
+        Library a = new Library();
+        for (int i = unique; i<=0; i ++){
+            a.count++;
+        }
+    }
 }
